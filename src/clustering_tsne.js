19b85418fe6c6e34a1089330b5d20e1faeed60d0
rolf <rvers@protonmail.com>
2017-12-15 14:00:31 +0100

    clustering and dimensionality reduction working

--- src/clustering_tsne.js
+++ src/clustering_tsne.js
@@ -1,33 +1,51 @@
-d3.csvParse()
-
-var colors = [
-    [20, 20, 80],
-    [22, 22, 90],
-    [250, 255, 253],
-    [0, 30, 70],
-    [200, 0, 23],
-    [100, 54, 100],
-    [255, 13, 8]
-];
-
-// Calculate clusters.
-var clusters = clusterfck.kmeans(colors, 3);
-//clusters.map(x => {console.log(x.length);  x.map(y => console.log(y));});
-
-var opt = {};
-opt.epsilon = 10; // epsilon is learning rate (10 = default)
-opt.perplexity = 30; // roughly how many neighbors each point influences (30 = default)
-opt.dim = 2; // dimensionality of the embedding (2 = default)
-
-var tsne = new tsnejs.tSNE(opt); // create a tSNE instance
-
-// initialize data.
-tsne.initDataRaw(data);
-
-for(var k = 0; k < 5; k++) {
-    console.log("check");
-    tsne.step(); // every time you call this, solution gets better
+function toNestedArray(data) {
+    return data.map(x => [x['x'], x['y'], x['u'], x['v'], x['m'], x['a']]);
 }
 
-var reduced_colors = tsne.getSolution(); // Y is an array of 2-D points that you can plot
-//reduced_colors.map(x => console.log(x));
+function clusterKMeans(data, numclusters) {
+    nested_array = toNestedArray(data);
+
+    var kmeans = new KMEANS();
+    var clusters = kmeans.run(nested_array, numclusters);
+
+    out = [];
+    for (var i = 0; i < clusters.length; i++) {
+        for (var j = 0; j < clusters[i].length; j++) {
+            out[clusters[i][j]] = i;
+        }
+    }
+
+    return out;
+}
+
+function reduceDimTSNE(data) {
+    var opt = {};
+    opt.epsilon = 10; // epsilon is learning rate (10 = default)
+    opt.perplexity = 30; // roughly how many neighbors each point influences (30 = default)
+    opt.dim = 2; // dimensionality of the embedding (2 = default)
+
+    var tsne = new tsnejs.tSNE(opt); // create a tSNE instance
+
+    // initialize data.
+    tsne.initDataRaw(data_boats);
+
+    for(var k = 0; k < 500; k++) {
+        tsne.step(); // every time you call this, solution gets better
+    }
+
+    return tsne.getSolution(); // an array of 2-D points that you can plot
+}
+
+
+var data_boats = toNestedArray(boat_data['boats']);
+var clusters = clusterKMeans(boat_data['boats']);
+var reduced_tsne = reduceDimTSNE(data_boats);
+
+var data_tsne = [];
+var ob = {};
+for (i = 0; i < reduced_tsne.length; i++) {
+    ob = {x:reduced_tsne[i][0], y:reduced_tsne[i][1], category:clusters[i]};
+    data_tsne.push(ob);
+}
+
+drawScatterplot(data_tsne);
