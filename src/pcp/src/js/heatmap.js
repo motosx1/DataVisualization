cb19eda0e477d4a0200afe148a159c25dd649968
Graur Dan <dangraur1@yahoo.com>
2017-12-22 23:36:20 +0100

    Added all the comments

--- src/pcp/src/js/heatmap.js
+++ src/pcp/src/js/heatmap.js
@@ -2,6 +2,12 @@ var margin = { top: 50, right: 0, bottom: 100, left: 30 };
 var colors = ['#b2182b','#d6604d','#f4a582','#fddbc7','#f7f7f7','#d1e5f0','#92c5de','#4393c3','#2166ac']; // alternatively colorbrewer.YlGnBu[9]
 
 
+/**
+ * Plot the heatmap
+ *
+ * @param a matrix containing the feautre correlations
+ * @param featureNames the names of the features in data which will be plotted
+ */
 window.drawHeatmap = function(data, featureNames) {
     var chart = d3.select('#headingFive');
     var targetWidth = chart.node().getBoundingClientRect().width - margin.left - margin.right;
@@ -9,6 +15,7 @@ window.drawHeatmap = function(data, featureNames) {
     var targetHeight = sqSize * featureNames.length + (3.5 * margin.bottom);
     var legendElementWidth = sqSize * 2;
 
+    /* Create an SVG element associated to the heatmap */
     var svg = d3
         .select("#corr-heatmap")
         .append("svg")
@@ -19,6 +26,7 @@ window.drawHeatmap = function(data, featureNames) {
             .attr("id", "g-heat-map")
             .attr("transform", "translate(" + margin.left + "," + (margin.top / 2) + ")");
 
+    /* Create the bottom feature labels */
     svg.selectAll(".featureLabelBottom")
         .data(featureNames)
         .enter()
@@ -29,6 +37,7 @@ window.drawHeatmap = function(data, featureNames) {
         .style("text-anchor", "end")
         .attr("transform", "translate(" + (sqSize * 3.5) + "," + (sqSize * featureNames.length + 5) +")rotate(-90)");
 
+    /* Create the left feature labels */
     svg.selectAll(".featureLabelLeft")
         .data(featureNames)
         .enter()
@@ -39,6 +48,7 @@ window.drawHeatmap = function(data, featureNames) {
         .style("text-anchor", "end")
         .attr("transform", "translate(" + (3 * sqSize - 5) + "," + margin.top +")");
 
+    /* Redundant, but still done: get the min and max values from the matrix */
     var matMax = d3.max(data, function(d, i) {
         return d[i];
     });
@@ -49,11 +59,13 @@ window.drawHeatmap = function(data, featureNames) {
 
     console.log("Max and min " + matMax + ", " + matMin);
 
+    /* Create a color scale which maps values in the interval [-1, 1] to the divergent color scheme */
     var colorScale = d3
             .scaleQuantile()
             .domain([-1, 1])
             .range(colors);
 
+    /* Create a rectangle for each of the unique pairs of features */
     for (var i = 0; i < featureNames.length; ++i)
         for (var j = 0; j <= i; ++j)
             svg
@@ -75,6 +87,7 @@ window.drawHeatmap = function(data, featureNames) {
 
     cards.exit().remove();
 
+    /* Create an element which corresponds to the legend */
     var legend = svg
         .selectAll(".legend")
         .data([0].concat(colorScale.quantiles()), function(d) { return d; })
@@ -82,6 +95,7 @@ window.drawHeatmap = function(data, featureNames) {
         .append("g")
         .attr("class", "legend");
 
+    /* For each of the bins, create a rectangular element in the legend, and add its color */
     legend
         .append("rect")
             .attr("x", function(d, i) { return legendElementWidth * i; })
@@ -90,6 +104,7 @@ window.drawHeatmap = function(data, featureNames) {
             .attr("height", sqSize / 2)
             .style("fill", function(d, i) { return colors[i]; });
 
+    /* Assign the labels to the legend */
     legend
         .append("text")
             .attr("class", "heat-font")
